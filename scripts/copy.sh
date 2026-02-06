49154086f38f81a74db61777eea65cc58b8e5dc9
Yago Martínez <yagomartinezdef@gmail.com>
2026-02-06 18:06:05 -0500

    Updates copy script
    Updates the copy script and eliminates irrelevant files that where being copied.

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -18,8 +18,76 @@ rm -rvf ./build-321xlr
 mkdir -p ./build-321xlr/src
 mkdir -p ./build-321xlr/out
 
+mkdir -p ./build-321xlr/out/qbitsim-aircraft-a321-253
+mkdir -p ./build-321xlr/out/qbitsim-aircraft-a321-253-lock-highlight
+
+mkdir -p ./build-321xlr/out/qbitsim-aircraft-a321-253/config/QBT_321N/a321-253ny
+mkdir -p ./build-321xlr/out/qbitsim-aircraft-a321-253/html_ui/CSS
+mkdir -p ./build-321xlr/out/qbitsim-aircraft-a321-253/html_ui/Fonts
+mkdir -p ./build-321xlr/out/qbitsim-aircraft-a321-253/html_ui/Images
+mkdir -p ./build-321xlr/out/qbitsim-aircraft-a321-253/html_ui/JS
+mkdir -p ./build-321xlr/out/qbitsim-aircraft-a321-253/html_ui/Pages/VCockpit/Instruments/Airliners/Qbit_A321_Neo/EFB
+mkdir -p ./build-321xlr/out/qbitsim-aircraft-a321-253/html_ui/Pages/VCockpit/Instruments/FlightElements
+mkdir -p ./build-321xlr/out/qbitsim-aircraft-a321-253/html_ui/Pages/VCockpit/Instruments/NavSystems
+mkdir -p ./build-321xlr/out/qbitsim-aircraft-a321-253/ModelBehaviorDefs/A32NX
+mkdir -p ./build-321xlr/out/qbitsim-aircraft-a321-253/effects
+
+# merge from FBW A32NX/base to A321xlr/base
+cp -r ./flybywire/fbw-a32nx/src/base/flybywire-aircraft-a320-neo/config/a32nx/a320-251n/. ./build-321xlr/out/qbitsim-aircraft-a321-253/config/QBT_321N/a321-253ny
+cp -r ./flybywire/fbw-a32nx/src/base/flybywire-aircraft-a320-neo/effects/. ./build-321xlr/out/qbitsim-aircraft-a321-253/effects
+cp -r ./flybywire/fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/CSS/. ./build-321xlr/out/qbitsim-aircraft-a321-253/html_ui/CSS
+cp -r ./flybywire/fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/Fonts/fbw-a32nx/. ./build-321xlr/out/qbitsim-aircraft-a321-253/html_ui/Fonts/QBT_321N
+cp -r ./flybywire/fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/Images/fbw-a32nx/. ./build-321xlr/out/qbitsim-aircraft-a321-253/html_ui/Images/QBT_321N
+cp -r ./flybywire/fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/JS/fbw-a32nx/. ./build-321xlr/out/qbitsim-aircraft-a321-253/html_ui/JS/QBT_321N
+cp -r ./flybywire/fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/Pages/A32NX_Utils/. ./build-321xlr/out/qbitsim-aircraft-a321-253/html_ui/Pages/QBT_321N_Utils
+cp -r ./flybywire/fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/A32NX/. ./build-321xlr/out/qbitsim-aircraft-a321-253/html_ui/Pages/VCockpit/Instruments/QBT_321N
+cp -r ./flybywire/fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/. ./build-321xlr/out/qbitsim-aircraft-a321-253/html_ui/Pages/VCockpit/Instruments/Airliners/Qbit_A321_Neo
+cp -r ./flybywire/fbw-a32nx/src/base/flybywire-aircraft-a320-neo/ModelBehaviorDefs/. ./build-321xlr/out/qbitsim-aircraft-a321-253/ModelBehaviorDefs
+cp -r ./flybywire/fbw-a32nx/src/base/.eslintrc.js ./build-321xlr/out
+
+# override with custom files from A321xlr/base
+cp -r ./qbt-321xlr/src/base/. ./build-321xlr/out
+
+# merge from FBW A32NX/behavior to A321xlr/behavior
+mkdir -p ./build-321xlr/src/behavior
+cp -r ./flybywire/fbw-a32nx/src/behavior/. ./build-321xlr/src/behavior
+cp -r ./qbt-321xlr/src/behavior/. ./build-321xlr/src/behavior
+
+# merge from FBW A32NX/fonts to A321xlr/fonts
+mkdir -p ./build-321xlr/src/fonts
+cp -r ./flybywire/fbw-a32nx/src/fonts/. ./build-321xlr/src/fonts
+
+# merge from FBW A32NX/localization to A321xlr/localization
+mkdir -p ./build-321xlr/src/localization
+cp -r ./flybywire/fbw-a32nx/src/localization/. ./build-321xlr/src/localization
+cp -r ./qbt-321xlr/src/localization/. ./build-321xlr/src/localization
+
+# merge from FBW A32NX/model to A321xlr/model
+# mkdir -p ./build-321xlr/src/model
+# cp -r ./flybywire/fbw-a32nx/src/model/. ./build-321xlr/src/model
+# cp -r ./qbt-321xlr/src/model/. ./build-321xlr/src/model
+
+# merge from FBW A32NX/systems to A321xlr/systems
+mkdir -p ./build-321xlr/src/systems
+cp -r ./flybywire/fbw-a32nx/src/systems/. ./build-321xlr/src/systems
+cp -r ./qbt-321xlr/src/systems/. ./build-321xlr/src/systems
+
+# merge from FBW A32NX/wasm to A321xlr/wasm
+mkdir -p ./build-321xlr/src/wasm
+mkdir -p ./build-321xlr/src/wasm/fbw_a320
+mkdir -p ./build-321xlr/src/wasm/fadec_a32nx
+mkdir -p ./build-321xlr/src/wasm/systems/a320_systems
+cp -r ./flybywire/fbw-a32nx/src/wasm/. ./build-321xlr/src/wasm
+cp -r ./qbt-321xlr/src/wasm/. ./build-321xlr/src/wasm
+
+# copy extra files
+cp -r ./qbt-321xlr/.env ./build-321xlr/.env
+cp -r ./qbt-321xlr/mach.config.js ./build-321xlr/mach.config.js
+
+: <<'END_COMMENT'
 # copy from FBW A32NX source and 737 into one src
 cp -r ./flybywire/fbw-a32nx/src/behavior/. ./build-321xlr/src/behavior
+
 cp -r ./flybywire/fbw-a32nx/src/fonts/. ./build-321xlr/src/fonts
 cp -r ./flybywire/fbw-a32nx/src/localization/. ./build-321xlr/src/localization
 cp -r ./flybywire/fbw-a32nx/src/systems/. ./build-321xlr/src/systems
@@ -34,33 +102,6 @@ cp -r ./flybywire/fbw-a32nx/src/wasm/systems/a320_systems_wasm/. ./build-321xlr/
 cp -r ./flybywire/fbw-a32nx/src/systems/instruments/. ./build-321xlr/src/systems/instruments
 cp -r ./flybywire/fbw-a32nx/src/wasm/extra-backend-a32nx/. ./build-321xlr/src/wasm/extra-backend-a32nx
 cp -r ./flybywire/fbw-a32nx/src/model/. ./build-321xlr/src/model
-
-mkdir -p ./build-321xlr/out/qbitsim-aircraft-a321-253
-mkdir -p ./build-321xlr/out/qbitsim-aircraft-a321-253-lock-highlight
-
-mkdir -p ./build-321xlr/out/qbitsim-aircraft-a321-253/html_ui/CSS
-mkdir -p ./build-321xlr/out/qbitsim-aircraft-a321-253/html_ui/Fonts
-mkdir -p ./build-321xlr/out/qbitsim-aircraft-a321-253/html_ui/Images
-mkdir -p ./build-321xlr/out/qbitsim-aircraft-a321-253/html_ui/JS
-mkdir -p ./build-321xlr/out/qbitsim-aircraft-a321-253/html_ui/Pages/VCockpit/Instruments/Airliners/Qbit_A321_Neo/EFB
-mkdir -p ./build-321xlr/out/qbitsim-aircraft-a321-253/html_ui/Pages/VCockpit/Instruments/FlightElements
-mkdir -p ./build-321xlr/out/qbitsim-aircraft-a321-253/html_ui/Pages/VCockpit/Instruments/NavSystems
-mkdir -p ./build-321xlr/out/qbitsim-aircraft-a321-253/ModelBehaviorDefs/A32NX
-mkdir -p ./build-321xlr/out/qbitsim-aircraft-a321-253/effects
-mkdir -p ./build-321xlr/out/qbitsim-aircraft-a321-253/config/QBT_321N/a321-253ny
-
-cp -r ./flybywire/fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/Fonts/fbw-a32nx/. ./build-321xlr/out/qbitsim-aircraft-a321-253/html_ui/Fonts/QBT_321N
-cp -r ./flybywire/fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/Images/fbw-a32nx/. ./build-321xlr/out/qbitsim-aircraft-a321-253/html_ui/Images/QBT_321N
-cp -r ./flybywire/fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/JS/fbw-a32nx/. ./build-321xlr/out/qbitsim-aircraft-a321-253/html_ui/JS/QBT_321N
-cp -r ./flybywire/fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/EFB/. ./build-321xlr/out/qbitsim-aircraft-a321-253/html_ui/Pages/VCockpit/Instruments/Airliners/Qbit_A321_Neo/EFB
-cp -r ./flybywire/fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/FCU/. ./build-321xlr/out/qbitsim-aircraft-a321-253/html_ui/Pages/VCockpit/Instruments/Airliners/Qbit_A321_Neo/FCU
-cp -r ./flybywire/fbw-a32nx/src/base/flybywire-aircraft-a320-neo/ModelBehaviorDefs/A32NX/. ./build-321xlr/out/qbitsim-aircraft-a321-253/ModelBehaviorDefs/A32NX   
-cp -r ./flybywire/fbw-a32nx/src/base/flybywire-aircraft-a320-neo/effects/. ./build-321xlr/out/qbitsim-aircraft-a321-253/effects
-cp -r ./flybywire/fbw-a32nx/src/base/flybywire-aircraft-a320-neo/config/a32nx/a320-251n/. ./build-321xlr/out/qbitsim-aircraft-a321-253/config/QBT_321N/a321-253ny
-cp -r ./flybywire/fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/FlightElements ./build-321xlr/out/qbitsim-aircraft-a321-253/html_ui/Pages/VCockpit/Instruments/FlightElements/qbt-321xlr
-cp -r ./flybywire/fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/NavSystems/A320_Neo ./build-321xlr/out/qbitsim-aircraft-a321-253/html_ui/Pages/VCockpit/Instruments/NavSystems/A320_Neo
-cp -r ./flybywire/fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/Pages/VLivery/Liveries/A32NX_Registration ./build-321xlr/out/qbitsim-aircraft-a321-253/html_ui/Pages/VLivery/Liveries/Registration/A32NX_Registration
-
 
 cp -r ./qbt-321xlr/.env ./build-321xlr/.env
 cp -r ./qbt-321xlr/mach.config.js ./build-321xlr/mach.config.js
@@ -79,4 +120,4 @@ cp -r ./qbt-321xlr/src/base/qbitsim-aircraft-a321-253/. ./build-321xlr/out/qbits
 chmod +x ./build-321xlr/src/wasm/fbw_a320/build.sh
 chmod +x ./build-321xlr/src/wasm/fadec_a32nx/build.sh
 #chmod +x ./build-321xlr/src/wasm/flypad-backend/build.sh
-
+END_COMMENT
