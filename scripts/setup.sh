e0c1245db8a357ca79be39a32405a2363f4f9073
Yago Martínez <yagomartinezdef@gmail.com>
2026-02-09 11:46:52 -0500

    Updates scripts and package.json
    Updates both the package.json eliminating deprecated blot and and bups scripts to work with FlyByWires V0.14 structure

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -13,13 +13,27 @@ fi
 
 cd /external
 
+PNPM_ACTION_TAKEN=false
+
 for arg in "$@"; do
   if [ "$arg" = "--clean" ]; then
     echo "Removing node_modules..."
     rm -rf node_modules/
+    PNPM_ACTION_TAKEN=true
+  elif [ "$arg" = "--debug" ]; then
+    echo "Removing node_modules..."
+    rm -rf node_modules/
+    rm -rf .pnpm-store/
+    pnpm i --loglevel verbose
+    PNPM_ACTION_TAKEN=true
   fi
 done
-pnpm i
+
+# If no specific pnpm-related action was taken by arguments, run default pnpm install
+if [ "$PNPM_ACTION_TAKEN" = "false" ]; then
+  echo "No specific setup arguments provided. Running default pnpm install."
+  pnpm i 
+fi
 
 # restore ownership (when run as github action)
 if [ "${GITHUB_ACTIONS}" == "true" ]; then
